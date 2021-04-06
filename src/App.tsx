ba8f5ec760d15477db35ba9f29b89a6dea6a0b91
huybn5776 <>
2021-04-06 23:17:03 +0800

    feat: move ime-input to InputPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+
 import './App.scss';
-import ImeTextArea from './components/ImeTextArea/ImeTextArea';
+import InputPage from './components/InputPage/InputPage';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <ImeTextArea />
+      <BrowserRouter>
+        <Route exact path="/" render={() => <Redirect to="/ime" />} />
+        <Route path="/ime">
+          <InputPage />
+        </Route>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/components/InputPage/InputPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ImeTextArea from '../ImeTextArea/ImeTextArea';
+import './InputPage.scss';
+
+const InputPage: React.FC = () => {
+  return (
+    <div className="InputPage">
+      <ImeTextArea/>
+    </div>
+  );
+};
+
+export default InputPage;
