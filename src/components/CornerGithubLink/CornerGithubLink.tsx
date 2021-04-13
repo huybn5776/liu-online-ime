70881d94be0ae04618e41533210397779ee6585f
huybn5776 <>
2021-04-13 23:11:53 +0800

    feat: add github icon

---
+++ src/components/CornerGithubLink/CornerGithubLink.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './CornerGithubLink.scss';
+
+const CornerGithubLink: React.FC = () => (
+  <a
+    className="corner-github-link"
+    href="https://github.com/huybn5776/liu-online-ime"
+    target="_blank"
+    rel="noreferrer noopener"
+  >
+    <i className="github icon" />
+  </a>
+);
+
+export default CornerGithubLink;
--- src/components/SettingsPage/SettingsPage.tsx
+++ src/components/SettingsPage/SettingsPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import AppLink from '../AppLink/AppLink';
+import CornerGithubLink from '../CornerGithubLink/CornerGithubLink';
 import GeneralSettings from '../GenerialSettings/GeneralSettings';
 import SettingTabContent from '../SettingTabContent/SettingTabContent';
 import UserDictSettings from '../UserDictSettings/UserDictSettings';
@@ -15,20 +16,23 @@ const SettingsPage: React.FC = () => {
   const [activeTab, setActiveTab] = useState(SettingTab.general);
 
   return (
-    <div>
+    <div className="SettingsPage">
       <div className="ui top attached tabular menu">
         {renderTabButton(SettingTab.general, '一般設定')}
         {renderTabButton(SettingTab.userDict, '加字加詞')}
         <AppLink className="icon-button setting-page-close-button" to="/" withParas>
-          <i className="close icon" />
+          <i className="close icon"/>
         </AppLink>
       </div>
       <SettingTabContent active={activeTab === SettingTab.general}>
-        <GeneralSettings />
+        <GeneralSettings/>
       </SettingTabContent>
       <SettingTabContent active={activeTab === SettingTab.userDict}>
-        <UserDictSettings />
+        <UserDictSettings/>
       </SettingTabContent>
+      <div className="github-link-container">
+        <CornerGithubLink/>
+      </div>
     </div>
   );
 
