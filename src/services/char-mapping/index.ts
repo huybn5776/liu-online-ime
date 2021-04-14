4e0feee3cc6909de31c87ad1ce2eb7ae314a7ad4
huybn5776 <>
2021-04-14 15:35:43 +0800

    feat: memorize loaded char mapping dict

--- src/services/char-mapping/index.ts
+++ src/services/char-mapping/index.ts
@@ -1,15 +1,31 @@
-import { EMPTY, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { EMPTY, Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 import { CharMappingDict } from '../../interfaces/char-mapping';
 import { loadCharMappingFromBoshiamy } from './boshiamy-char-mapping-service';
 import { loadCinCharMapping } from './cin-char-mapping-service';
 import { appendUserDict } from './user-dict-char-mapping-service';
 
+const lazyCharMappingDict: () => Observable<CharMappingDict> = (() => {
+  let loadedCharMappingDict: CharMappingDict | null = null;
+
+  const load: () => Observable<CharMappingDict> = () => {
+    return loadCinCharMapping() || loadCharMappingFromBoshiamy() || EMPTY;
+  };
+
+  return () => {
+    if (loadedCharMappingDict) {
+      return of(loadedCharMappingDict);
+    }
+
+    return load().pipe(
+      tap((charMappingDict) => {
+        loadedCharMappingDict = charMappingDict;
+      }),
+    );
+  };
+})();
+
 export function loadCharMappings(): Observable<CharMappingDict> {
-  const source = loadCinCharMapping() || loadCharMappingFromBoshiamy();
-  if (source) {
-    return source.pipe(map(appendUserDict));
-  }
-  return EMPTY;
+  return lazyCharMappingDict().pipe(map(appendUserDict));
 }
